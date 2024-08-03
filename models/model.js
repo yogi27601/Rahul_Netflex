5d119392f8ce35b601eda5f286b1a1822aaadcb3
yogi27601 <yogi27601@gmail.com>
2024-08-03 04:56:42 -0700

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,7 +1,7 @@
 const mongoose= require("mongoose")
 async function main(){
     try {
-        await mongoose.connect("mongodb+srv://yogi27601:Rahul@27601@cluster0.ztpvtfm.mongodb.net/")
+        await mongoose.connect("mongodb+srv://yogi27601:Rahul%4027601@cluster0.ztpvtfm.mongodb.net/mydatabase1")
         console.log("database connected successfully with server ")
         
     } catch (error) {
